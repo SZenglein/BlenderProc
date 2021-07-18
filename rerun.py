57536a41636fe461d24fef90f51a0f9f842b8fa1
Sascha <sascha@fedoradesktop.fritz.box>
2021-07-18 14:13:07 +0200

    add skip functionality to rerun.py

--- rerun.py
+++ rerun.py
@@ -5,27 +5,33 @@ import random
 
 parser = argparse.ArgumentParser("Rerun script for BlenderProc")
 parser.add_argument("--seed",  help="Seed used for the generation")
+parser.add_argument("--skip",  help="Skip the first n BlenderProc runs", default=0, type=int)
+parser.add_argument("--runs",  help="Run the BlenderProc pipeline n times", default=100, type=int)
 parser.add_argument("params", help="The params which are handed over to the run.py script", nargs='+')
 args = parser.parse_args()
 
 seed = random.randint(0, 1000000)
 env = os.environ
 if args.seed:
-    seed = args.seed
+    seed = int(args.seed)
 
 print("running rerun.py with seed", seed)
 random.seed(seed)
 
 # this sets the amount of runs, which are performed
-amount_of_runs = 100
+amount_of_runs = args.runs
+skip_runs = args.skip
 
 # set the folder in which the run.py is located
 rerun_folder = os.path.abspath(os.path.dirname(__file__))
 
+for run_id in range(skip_runs):
+    random.randint(0, 1000000)
+
 # the first one is the rerun.py script, the last is the output
 used_arguments = list(args.params)
 output_location = os.path.abspath(used_arguments[-1])
-for run_id in range(amount_of_runs):
+for run_id in range(skip_runs, amount_of_runs):
     run_seed = random.randint(0, 1000000)
     # in each run, the arguments are reused
     cmd = ["python", os.path.join(rerun_folder, "run.py")]
